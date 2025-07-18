c142fa326bcac045973b2634152a2d1a3da0b30b
Sangram Das <sangramraju143@gmail.com>
2025-07-18 15:11:07 +0530

    all files

---
+++ Background_Removal/server/controllers/userControllers.js
@@ -0,0 +1,6 @@
+// API Controller function to manage CLeark User With database
+// http://localhost:4000/api/user/webhooks
+
+const clerkWebhooks = async (req,res)=>{
+    
+}
---
+++ Background_Removal/server/server.js
@@ -0,0 +1,27 @@
+import "dotenv/config"
+import express from 'express'
+import cors from 'cors'
+import connectDB from "./configs/mongodb.js"
+
+// App Config
+const PORT = process.env.PORT || 4000
+
+const app = express()
+await connectDB()
+
+
+//Initalize Middlewares
+app.use(express.json())
+app.use(cors())
+
+
+
+// API routes
+app.get('/', (req,res)=> res.send("API is Working"))
+
+
+
+
+
+app.listen(PORT,()=> console.log("Server running on port " +PORT));
+
